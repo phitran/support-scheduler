5fe008eb6e7f948c14a1d932fbc8fa17b90a90e9
Phi Tran <phi.long.tran@gmail.com>
2016-02-20 23:05:14 -0800

    initial api & readme

--- README.md
+++ README.md
@@ -5,3 +5,63 @@ Support Scheduler
 brew install mongodb
 node
 npm install
+
+
+API
+GET /services/users
+Returns array of users
+[{"_id":"56c8a0273084cd793ca7eb9e","name":"Sherry","username":"Sherry","password":"test"},...]
+
+GEt /services/users/{userId}
+Returns object of user
+{"_id":"56c8a0273084cd793ca7eb9e","name":"Sherry","username":"Sherry","password":"test"}
+
+GET /services/calendar?startDate=&endDate=
+//returns array of dates
+startDate/endDate needs to be ISO 8601 format
+[{"_id":"56c8a0283084cd793ca7ed32","date":"2016-12-31T08:00:00.000Z","month":"12","day":"31","year":"2016","isWeekend":true,"isHoliday":false,"description":""},...]
+
+GEt /services/schedule?month=
+returns array of schedule of a month
+month is required
+
+GET /services/schedule/{userId}?month=
+returns array of schedule of a month for a user
+month is required
+[{ _id: 56c8d4e1446df3ba438e45d1,
+     userId: 56c8d4e1446df3ba438e45a9,
+     calendarId: 56c8d4e1446df3ba438e4442,
+     supportHero_userId: 56c8d4e1446df3ba438e45a9,
+     day: 
+      { _id: 56c8d4e1446df3ba438e4442,
+        date: Sun Jan 31 2016 00:00:00 GMT-0800 (PST),
+        month: 1,
+        day: 31,
+        year: 2016,
+        isWeekend: false,
+        isHoliday: false,
+        description: '' },
+     suppertHero: 
+      { _id: 56c8d4e1446df3ba438e45a9,
+        name: 'Kevin',
+        username: 'Kevin',
+        password: 'test' },
+     user: 
+      { _id: 56c8d4e1446df3ba438e45a9,
+        name: 'Kevin',
+        username: 'Kevin',
+        password: 'test' } }, ...]
+
+
+POST /services/schedule/swap
+payload: {
+    originSchedule
+    targetSchedule
+}
+Ids of target. 
+Return update values as object
+
+POST /services/schedule/undoable
+payload: {
+    schedule: schedule object
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,345 @@
-module.exports = [{
-    method: ['GET'],
-    path: '/test',
-    config: {
+'use strict';
+
+const Boom = require('boom');
+const Joi = require('joi');
+const _get = require('lodash/get');
+const _random = require('lodash/random');
+const mongo = require('mongodb');
+const mongoClient = mongo.MongoClient;
+const mongoIdValidation = [Joi.string().hex().length(24).optional(), Joi.string().length(12).optional()];
+const url = 'mongodb://localhost:27017/support-schedule';
+const moment = require('moment');
+
+function queryCollection(collection, options) {
+    return new Promise((resolve, reject) => {
+        mongoClient.connect(url, (err, db) => {
+            if (err) {
+                reject(Boom.badRequest(err));
+            }
+
+            db.collection(collection).find(options).toArray((err, items) => {
+                if (err) {
+                    db.close();
+                    reject(Boom.badRequest(err));
+                }
+
+                db.close();
+                resolve(items);
+            });
+        });
+    });
+}
+
+function updateDocument(collection, filter, options) {
+    return new Promise((resolve, reject) => {
+        mongoClient.connect(url, (err, db) => {
+            if (err) {
+                reject(Boom.badRequest(err));
+            }
+
+            return db.collection(collection).updateOne(filter, options).then(value => {
+                return db.collection(collection).find(filter).limit(1).next((err, document) => {
+                    if (err) {
+                        reject(Boom.badRequest(err));
+                    }
+
+                    resolve(document);
+                    db.close();
+                });
+            }, err => {
+                reject(err);
+                db.close();
+            });
+        });
+    });
+}
+
+function aggregateSchecduleCollection(matchers) {
+    let pipeline = [
+        {
+            $lookup: {
+                from: 'calendar',
+                localField: 'calendarId',
+                foreignField: '_id',
+                as: 'day'
+            }
+        },
+        {
+            $unwind: '$day'
+        },
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'userId',
+                foreignField: '_id',
+                as: 'user'
+            }
+        },
+        {
+            $unwind: '$user'
+        }
+    ];
+
+    //merge matchers into pipline
+    pipeline = pipeline.concat(matchers);
+
+    return new Promise((resolve, reject) => {
+        mongoClient.connect(url, function (err, db) {
+            if (err) {
+                reject(Boom.badRequest(err))
+            }
+
+            db.collection('schedule').aggregate(pipeline, (err, result) => {
+                if (err) {
+                    reject(Boom.badRequest(err))
+                }
+
+                db.close();
+                resolve(result);
+            })
+        });
+    });
+}
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/services/user/{userId*}',
+        config: {
+            validate: {
+                query: {}, //disallow any query params
+                params: {
+                    userId: mongoIdValidation
+                }
+            }
+        },
         handler: function (request, reply) {
-            return reply('yay2');
+            const userId = _get(request.params, 'userId');
+            const options = userId ? {"_id": new mongo.ObjectID(userId)} : {};
+
+            queryCollection('users', options).then(value => {
+                const payload = userId ? value[0] : value;
+                reply(payload).code(200);
+            }, err => {
+                reply(Boom.badRequest(err));
+            });
+        }
+    },
+    {
+        method: 'GET',
+        path: '/services/calendar',
+        config: {
+            validate: {
+                query: {
+                    startDate: Joi.date().iso().required(),
+                    endDate: Joi.date().iso().required()
+                }
+            }
+        },
+        handler: function (request, reply) {
+            const options = {
+                date: {
+                    $gte: new Date(request.url.query.startDate),
+                    $lte: new Date(request.url.query.endDate)
+                }
+            };
+
+            queryCollection('calendar', options).then(value => {
+                reply(value).code(200);
+            }, err => {
+                reply(Boom.badRequest(err));
+            });
+        }
+    },
+    {
+        method: 'GET',
+        path: '/services/schedule/{userId*}',
+        config: {
+            validate: {
+                query: {
+                    month: Joi.number().integer().required()
+                },
+                params: {
+                    userId: mongoIdValidation
+                }
+            }
+        },
+        handler: function (request, reply) {
+            const userId = _get(request.params, 'userId');
+            const getScheduleMatcher = [
+                {
+                    $match: {
+                        "day.month": request.query.month
+                    }
+                }
+            ];
+
+            if (userId) {
+                getScheduleMatcher.push({
+                    $match: {
+                        "userId": new mongo.ObjectID(userId)
+                    }
+                });
+            }
+
+            aggregateSchecduleCollection(getScheduleMatcher).then(result => {
+                reply(result).code(200);
+            }, err => {
+                reply(Boom.badRequest(err));
+            });
+        }
+    },
+    {
+        method: 'POST',
+        path: '/services/schedule/swap',
+        config: {
+            validate: {
+                query: {},
+                payload: {
+                    originSchedule: Joi.object().keys({
+                        _id: mongoIdValidation,
+                        userId: mongoIdValidation,
+                        calendarId: mongoIdValidation
+                    }),
+                    targetSchedule: Joi.object().keys({
+                        _id: mongoIdValidation,
+                        userId: mongoIdValidation,
+                        calendarId: mongoIdValidation
+                    })
+                }
+            }
+        },
+        handler: function (request, reply) {
+            const origin = request.payload.originSchedule;
+            const target = request.payload.targetSchedule;
+
+            const promises = [
+                updateDocument('schedule', {_id: new mongo.ObjectID(origin._id)}, {$set: {userId: new mongo.ObjectID(target.userId)}}),
+                updateDocument('schedule', {_id: new mongo.ObjectID(target._id)}, {$set: {userId: new mongo.ObjectID(origin.userId)}})
+            ];
+
+            Promise.all(promises).then(values => {
+                var response = {
+                    originSchedule: values[0],
+                    targetSchedule: values[1]
+                };
+
+                reply(response).code(200);
+            });
+        }
+    },
+    {
+        method: 'POST',
+        path: '/services/schedule/undoable',
+        config: {
+            validate: {
+                query: {},
+                params: {
+                    schedule: {
+                        _id: mongoIdValidation,
+                        userId: mongoIdValidation,
+                        calendarId: mongoIdValidation
+                    }
+                }
+            }
+        },
+        handler: function (request, reply) {
+            /**
+             * get current Schedule (month, year)
+             * get all schedules in month
+             * get all users not scheduled in current Month
+             * pick random
+             */
+            const idMatcher = [
+                {
+                    $match: {
+                        "_id": new mongo.ObjectID(request.payload.schedule._id)
+                    }
+                }
+            ];
+            let requestedSchedule = null;
+
+            //get current schedule from request
+            aggregateSchecduleCollection(idMatcher)
+                .then(currentSchedule => {
+                    requestedSchedule = currentSchedule[0];
+                    const montYearMatcher = [
+                        {
+                            $match: {
+                                "day.month": requestedSchedule.day.month
+                            }
+                        },
+                        {
+                            $match: {
+                                "day.year": requestedSchedule.day.year
+                            }
+                        }
+                    ];
+
+                    //get schedule for current month/year
+                    return aggregateSchecduleCollection(montYearMatcher);
+                })
+                .then(currentMonthSchedules => {
+                    //create array of UserIds
+                    const currentMonthUserIds = [];
+                    currentMonthSchedules.forEach((value, index) => {
+                        currentMonthUserIds.push(value.user._id)
+                    });
+
+                    //get all users not schedule for current month
+                    return queryCollection('users', {_id: {$nin: currentMonthUserIds}});
+                })
+                .then(poolOfUser => {
+                    //create array of UserIds
+                    const poolOfUserId = [];
+                    poolOfUser.forEach((value, index) => {
+                        poolOfUserId.push(value._id);
+                    });
+
+                    const poolOfUserSchedulesMatcher = [
+                        {
+                            $match: {
+                                userId: {
+                                    $in: poolOfUserId
+                                }
+                            }
+                        },
+                        {
+                            $match: {
+                                "day.month": requestedSchedule.day.month === 12 ? 1 : requestedSchedule.day.month + 1
+                            }
+                        },
+                        {
+                            $match: {
+                                "day.year": requestedSchedule.day.month === 12 ? requestedSchedule.day.year + 1 : requestedSchedule.day.year
+                            }
+                        }
+                    ];
+
+                    //get next months schedule for pool of users
+                    return aggregateSchecduleCollection(poolOfUserSchedulesMatcher);
+                })
+                .then(poolOfUserSchedules => {
+                    //pick random user from pool and swap times
+                    const randomUser = _random(0, poolOfUserSchedules.length);
+                    const promises = [
+                        updateDocument('schedule', {_id: requestedSchedule._id}, {$set: {userId: poolOfUserSchedules[randomUser].userId}}),
+                        updateDocument('schedule', {_id: poolOfUserSchedules[randomUser]._id}, {$set: {userId: requestedSchedule.userId}})
+                    ];
+
+                    Promise.all(promises).then(values => {
+                        var response = {
+                            originalSchedule: values[0],
+                            newSchedule: values[1]
+                        };
+
+                        reply(response).code(200);
+                    });
+                })
+                .catch(err => {
+                    reply(Boom.badRequest(err));
+                });
         }
     }
-}];
+
+];
